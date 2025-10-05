589099e1d2277574e20094f2ad03aa970aefb256
lraj22 <63673413+lraj22@users.noreply.github.com>
2025-10-04 22:15:13 -0700

    Clicking cards reveals action w/ limit

--- src/main.js
+++ src/main.js
@@ -40,21 +40,55 @@ window.btnAction = function btnAction (actionId) {
 	if (possibleOverlay !== null) overlayMessage.innerHTML = possibleOverlay;
 };
 
+function getNeutral () {
+	let possibilities = ["nothing"];
+	return possibilities[Math.floor(Math.random() * possibilities.length)];
+}
+
 function startRound (roundNumber) {
 	console.trace(`round ${roundNumber} started!`)
 	currentRoundNumber = roundNumber;
 	let rdataOptions = roundData[roundNumber].actions.slice();
 	rdataOptions.push("death");
 	rdataOptions = rdataOptions.slice(-16);
-	let options = new Array(16).fill().map((_, i) => rdataOptions[i] || "neutral");
+	let options = new Array(16).fill().map((_, i) => rdataOptions[i] || getNeutral());
 	cardActions = shuffleArray(options);
+	console.log(cardActions);
 	let labels = shuffleArray(roundData[roundNumber].labels);
 	selectCount = roundData[roundNumber].selectCount;
 	document.querySelectorAll("[data-card-id]").forEach((card, i) => {
 		card.textContent = labels[i];
+		card.title = labels[i];
 		card.classList.remove("flipped");
 	});
 	overlayMessage.innerHTML = "";
+}
+
+function endRound () {
+	dom.cards.classList.add("roundEnded");
+	dom.roundEnded.textContent = "Ended. You may not select any more cards.";
+	// here, give user options to either "exit the game" and keep their earnings, or "continue to the next round"
+	// maybe later, implement the ability to pick from two continue options
+}
+
+function getActionDetails (action) {
+	let name = "none";
+	let classification = "none";
+	
+	if (/^[+-]\d+$/.test(action)) { // +1, +10, -1, -10, etc.
+		name = ((action[0] === "+") ? "Gain" : "Lose") + ` ${action.slice(1)} points`;
+		classification = ((action[0] === "+") ? "good" : "bad");
+	} else if (action === "nothing") {
+		name = "Literally nothing :P";
+		classification = "neutral";
+	} else if (action === "death") {
+		name = "Death.";
+		classification = "death";
+	} else {
+		name = "Unknown action";
+		classification = "neutral";
+	}
+	return { action, name, classification };
 }
 
 function doCardAction (action) {
@@ -62,11 +96,18 @@ function doCardAction (action) {
 }
 
 document.querySelectorAll("[data-card-id]").forEach((card, index) => {
-	index; // 1 - 16, not 0 - 15
 	card.addEventListener("click", _ => {
 		if (overlayMessage.innerHTML) return; // if overlay open, you can't click cards
 		if (card.classList.contains("flipped")) return; // can't flip same card twice
+		let currentSelectedCount = document.querySelectorAll("[data-card-id].flipped").length;
+		if (currentSelectedCount >= selectCount) return; // can't select more than allowable
+		
+		let { action, name, classification } = getActionDetails(cardActions[index]);
 		card.classList.add("flipped");
-		doCardAction(cardActions[index]);
+		card.classList.add(classification);
+		card.textContent = name;
+		doCardAction(action);
+		currentSelectedCount++;
+		if (currentSelectedCount >= selectCount) endRound(); // we now reached maximum selectable, end the round
 	});
 });
