3f650837031ee69409a30806f92c9063b502f7de
lraj22 <63673413+lraj22@users.noreply.github.com>
2025-10-05 00:18:11 -0700

    Cards DO stuff now; added [next round]

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
 // overlays.js - includes various overlay messages and actions, so they don't clutter up main.js
 
 const overlays = {
-	"round1": `<p>Round 1.</p><p>3 good cards, 3 bad cards, 1 Death. Pick 5 cards.</p><p>By the way, Death will always be one (and only one) of the cards.</p>[Begin|startRound1]`
+	"round1": `<p>Round 1.</p><p>3 good cards, 3 bad cards, 1 Death. Pick 5 cards.</p><p>By the way, Death will always be one (and only one) of the cards.</p>[Begin|startRound(1)]`,
+	"round2": `<p>Cards will have random text on them, you can choose whichever ones you like... :]</p><p>Introducing more powerful cards, for example double and half! 3 good, 3 bad, 1 Death as always. Pick 3.</p>[Begin|startRound(2)]`,
 };
 
 export const roundData = {
@@ -10,11 +11,16 @@ export const roundData = {
 		"selectCount": 5,
 		"actions": ["+10", "+15", "+20", "-5", "-10", "-15"],
 	},
+	"2": {
+		"labels": ["@Olive", "@One For Freedom", "@Lakshya Raj", "@atomtables", "@sebastian", "@Rino", "@Azzy", "@PianoMan0", "@nikoentity", "@Mustafa", "@Anti-Bailey", "@julia do", "@i-am-unknown-81514525", "@Hannah", "@jollyroger182", "@kzlpndx"],
+		"selectCount": 3,
+		"actions": ["-10", "-30", "+10", "+30", "/2", "x2"],
+	},
 };
 
 export function getOverlay (id) {
 	if (!(id in overlays)) return null;
 	let message = overlays[id];
-	message = message.replace(/\[(.+)\|(.+)\]/, `<button type="button" onclick="btnAction('$2')">$1</button>`)
+	message = message.replace(/\[(.+)\|(.+)\]/, `<button type="button" onclick="$2">$1</button>`)
 	return message;
 }
--- src/main.js
+++ src/main.js
@@ -8,7 +8,12 @@ let currentRoundNumber = 0;
 let cardActions = new Array(16).fill("none");
 let selectCount = Infinity;
 let coins = 50;
+let isRoundActive = false;
 function setCoins (value) {
+	if (value < 0) {
+		endRound("notEnoughCoins");
+		value = 0;
+	}
 	coins = value;
 	dom.coins.textContent = value;
 }
@@ -17,9 +22,6 @@ setCoins(50);
 const actions = {
 	"closeOverlay": function () {
 		overlayMessage.innerHTML = "";
-	},
-	"startRound1": function () {
-		startRound(1);
 	},
 };
 
@@ -45,31 +47,62 @@ function getNeutral () {
 	return possibilities[Math.floor(Math.random() * possibilities.length)];
 }
 
-function startRound (roundNumber) {
-	console.trace(`round ${roundNumber} started!`)
+window.startRound = function startRound (roundNumber) {
+	let data = {};
+	if (roundNumber in roundData) {
+		data = roundData[roundNumber];
+	} else {
+		data = {
+			"labels": ["people", "history", "way", "art", "world", "information", "map", "two", "family", "government", "health", "system", "computer", "meat", "year", "thanks", "music", "person", "reading", "method", "data", "food", "understanding", "theory", "law", "bird", "literature", "problem", "software", "control", "knowledge", "power", "ability", "economics", "love", "internet", "television", "science", "library", "nature", "fact", "product", "idea", "temperature", "investment", "area", "society", "time", "work", "film", "water", "money", "example", "while", "business", "study", "game", "life", "form", "air", "day", "place", "number", "part", "field", "fish", "back", "process", "heat", "hand", "experience", "job", "book", "end", "point", "type", "home", "economy", "value", "body", "market", "guide", "interest", "state", "radio", "course", "company", "price", "size", "card", "list", "mind", "trade", "line", "care", "group", "risk", "word", "fat", "force", "key", "light", "training", "name", "school", "top", "amount", "level", "order", "practice", "research", "sense", "service", "piece"], // supposedly some of the most common nouns in English, 16 will be picked at random
+			"selectCount": Math.floor(Math.random() * 3) + 3,
+			"actions": ["-10", "-30", "-50", "+10", "+30", "+50", "/2", "x2", "/4", "x4"],
+		};
+	}
+	
+	console.trace(`round ${roundNumber} started!`);
 	currentRoundNumber = roundNumber;
-	let rdataOptions = roundData[roundNumber].actions.slice();
+	let rdataOptions = data.actions.slice();
 	rdataOptions.push("death");
 	rdataOptions = rdataOptions.slice(-16);
 	let options = new Array(16).fill().map((_, i) => rdataOptions[i] || getNeutral());
 	cardActions = shuffleArray(options);
 	console.log(cardActions);
-	let labels = shuffleArray(roundData[roundNumber].labels);
-	selectCount = roundData[roundNumber].selectCount;
+	let labels = shuffleArray(data.labels);
+	selectCount = data.selectCount;
 	document.querySelectorAll("[data-card-id]").forEach((card, i) => {
 		card.textContent = labels[i];
 		card.title = labels[i];
 		card.classList.remove("flipped");
 	});
 	overlayMessage.innerHTML = "";
+	
+	isRoundActive = true;
 }
 
-function endRound () {
+function endRound (reason) {
+	if (!reason) reason = "none";
+	if (!isRoundActive) return;
+	isRoundActive = false;
 	dom.cards.classList.add("roundEnded");
-	dom.roundEnded.textContent = "Ended. You may not select any more cards.";
-	// here, give user options to either "exit the game" and keep their earnings, or "continue to the next round"
+	const reasons = {
+		"none": "",
+		"death": "<b>You died. </b>",
+		"notEnoughCoins": "<b>You ran out of coins. </b>",
+	}
+	let endedMessage = `<p>${reasons[reason]}Round over. You may not select any more cards.</p>`;
+	
+	if (reason === "none") {
+		endedMessage += `<button type="button" class="btnDanger" onclick="endGame()">End the game here</button><button type="button" class="btnGood" onclick="startRound(${currentRoundNumber + 1})">Proceed to round ${currentRoundNumber + 1}</button>`;
+	} else {
+		endedMessage += `<button type="button" class="btnGood" onclick="endGame()">End the game here</button>`;
+	}
+	dom.roundEnded.innerHTML = endedMessage;
 	// maybe later, implement the ability to pick from two continue options
 }
+
+window.endGame = function endGame () {
+	alert("Not implemented yet!");
+};
 
 function getActionDetails (action) {
 	let name = "none";
@@ -93,6 +126,16 @@ function getActionDetails (action) {
 
 function doCardAction (action) {
 	console.log(action);
+	if (/^[+-]\d+$/.test(action)) { // +1, +10, -1, -10, etc.
+		setCoins(coins + (parseInt(action.slice(1)) * ((action[0] === "+") ? 1 : -1))); // add/remove coins based on
+	} else if (action === "nothing") {
+		// do nothing, lol
+	} else if (action === "death") {
+		endRound("death");
+		selectCount = 0;
+	} else {
+		console.warn(`Could not complete unidentified action: ${action}`);
+	}
 }
 
 document.querySelectorAll("[data-card-id]").forEach((card, index) => {
